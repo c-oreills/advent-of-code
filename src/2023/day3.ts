717c033fb62e73664df2171499393bdbf47e7d6c
Christy O'Reilly <christy@oreills.co.uk>
2024-01-22 21:48:31 +0000

    Day 3: Refactor to use const rather than let

--- src/2023/day3.ts
+++ src/2023/day3.ts
@@ -4,14 +4,14 @@ import assert from 'node:assert/strict';
 const lines = readFileSync('src/2023/inputs/3.txt', 'utf8').split('\n');
 
 function sumPartNumbers() {
-    let partNumbers = findPartNumbers(lines);
+    const partNumbers = findPartNumbers(lines);
     return partNumbers.reduce((acc, current) => acc + current, 0)
 }
 
 function findPartNumbers(lines: string[]) {
-    let partNumbers: number[] = [];
+    const partNumbers: number[] = [];
 
-    for (let [matchedText, lineIndex, matchIndex, matchLength] of numberMatchesGenerator(lines)) {
+    for (const [matchedText, lineIndex, matchIndex, matchLength] of numberMatchesGenerator(lines)) {
         if (!adjacentSymbolGenerator(lines, lineIndex, matchIndex, matchLength).next().done) {
             partNumbers.push(parseInt(matchedText))
         }
@@ -21,12 +21,12 @@ function findPartNumbers(lines: string[]) {
 }
 
 function* numberMatchesGenerator(lines: string[]): Generator<[string, number, number, number]> {
-    for (let [lineIndex, line] of lines.entries()) {
-        let numberMatches = [...line.matchAll(/\d+/g)]
-        for (let numberMatch of numberMatches) {
-            let matchedText = numberMatch[0];
-            let matchIndex = numberMatch.index as number;
-            let matchLength = matchedText.length;
+    for (const [lineIndex, line] of lines.entries()) {
+        const numberMatches = [...line.matchAll(/\d+/g)]
+        for (const numberMatch of numberMatches) {
+            const matchedText = numberMatch[0];
+            const matchIndex = numberMatch.index as number;
+            const matchLength = matchedText.length;
 
             yield [matchedText, lineIndex, matchIndex, matchLength]
         }
@@ -37,19 +37,19 @@ function* adjacentSymbolGenerator(
     lines: string[], lineIndex: number, matchIndex: number,
     matchLength: number
 ): Generator<[string, number, number]> {
-    let minCharIndex = Math.max(matchIndex - 1, 0)
-    let maxCharIndex = Math.min(matchIndex + matchLength + 1, lines[0].length - 1);
+    const minCharIndex = Math.max(matchIndex - 1, 0)
+    const maxCharIndex = Math.min(matchIndex + matchLength + 1, lines[0].length - 1);
 
-    for (let searchLineIndex of [lineIndex - 1, lineIndex, lineIndex + 1]) {
+    for (const searchLineIndex of [lineIndex - 1, lineIndex, lineIndex + 1]) {
         if (searchLineIndex < 0 || searchLineIndex >= lines.length) {
             continue
         }
-        let searchStr = lines[searchLineIndex].slice(minCharIndex, maxCharIndex);
-        for (let match of searchStr.matchAll(/[^\d.]/g)) {
+        const searchStr = lines[searchLineIndex].slice(minCharIndex, maxCharIndex);
+        for (const match of searchStr.matchAll(/[^\d.]/g)) {
             assert(match.index !== undefined)
 
-            let matchedText = match[0];
-            let columnIndex = minCharIndex + match.index;
+            const matchedText = match[0];
+            const columnIndex = minCharIndex + match.index;
             yield [matchedText, searchLineIndex, columnIndex];
         }
     }

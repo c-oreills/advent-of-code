ef6ea9cdf7cfb3de228c8de0f15eb0c689737033
Christy O'Reilly <christy@oreills.co.uk>
2024-01-23 22:02:07 +0000

    Day 4: Implement Part 2

--- src/2023/day4.ts
+++ src/2023/day4.ts
@@ -7,7 +7,12 @@ function sumScratchcardPoints() {
     return lines.reduce((acc, line) => acc + getScratchcardPoints(line), 0)
 }
 
-function getScratchcardPoints(line: string): number {
+function sumScratchcardCopies() {
+    const scratchcardCopies = calculateScratchcardCopies(lines);
+    return scratchcardCopies.reduce((acc, copies) => acc + copies, 0)
+}
+
+function getScratchcardMatchCount(line: string): number {
     const match = /^Card +\d+: ([\d ]+) \| ([\d ]+)$/.exec(line);
     assert(match)
     const [_, winningStr, haveStr] = match;
@@ -19,16 +24,37 @@ function getScratchcardPoints(line: string): number {
     // Node does not support Set.intersection or could have used Sets
     // const overlapCount = new Set(winningNumbers).intersection(haveNumbers)
 
-    const overlapCount = haveNumbers.filter((v) => winningNumbers.includes(v)).length
+    return haveNumbers.filter((v) => winningNumbers.includes(v)).length
+}
 
-    if (overlapCount === 0) {
+function getScratchcardPoints(line: string): number {
+    const matchCount = getScratchcardMatchCount(line);
+    if (matchCount === 0) {
         return 0;
     }
-    return Math.pow(2, overlapCount-1)
+    return Math.pow(2, matchCount-1)
+}
+
+function calculateScratchcardCopies(lines: string[]): number[] {
+    // We never actually parse Card ids but the main thing that matters is the
+    // sequence, not the index
+    const scratchcardMatchesAndCounts = lines.map((l) => ({
+        matchCount: getScratchcardMatchCount(l), cardCount: 1}));
+    for (let i = 0; i < scratchcardMatchesAndCounts.length; i++) {
+        const {matchCount, cardCount} = scratchcardMatchesAndCounts[i];
+        for (let j = 0; j < matchCount; j++) {
+            const sc = scratchcardMatchesAndCounts[i+j+1];
+            if (sc) {
+                sc.cardCount += cardCount;
+            }
+        }
+    }
+    return scratchcardMatchesAndCounts.map(({cardCount}) => cardCount) 
 }
 
 if (!module?.parent) {
     console.log(sumScratchcardPoints())
+    console.log(sumScratchcardCopies())
 }
 
-module.exports = { getScratchcardPoints }
+module.exports = { getScratchcardPoints: getScratchcardPoints, calculateScratchcardCopies }
